23e21a08d4551bb37542f476bb22ff003da259a6
Jackeline Lima <jackelinelimap@gmail.com>
2022-10-19 16:52:26 -0300

    criando componentes

--- list_task/lib/main.dart
+++ list_task/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:list_task/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const ListApp());
@@ -16,7 +18,33 @@ class ListApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+  String title = '';
+  String value = '';
+
+  MyHomePage({super.key});
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 100.34,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 60.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Conta de Internet',
+      value: 200.00,
+      date: DateTime.now(),
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -25,17 +53,101 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          //Container de gráfico
           Container(
-            width: double.infinity,
+            // width: double.infinity,
             child: Card(
               child: Text('Gráfico'),
               elevation: 5,
             ),
           ),
+          //Coluna do card com valores e informacoes
+          Column(
+            children: _transactions.map((tr) {
+              return Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ' + tr.value.toStringAsFixed(2),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('d MMM y').format(tr.date),
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 214, 212, 212),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            }).toList(),
+          ),
+          //Card Text Field
           Card(
-            child: Text('Lista de transaçoes'),
-          )
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  TextField(
+                    // onChanged: (newValue) => value = newValue,
+                    controller: titleController,
+                    decoration: InputDecoration(labelText: 'Título'),
+                  ),
+                  TextField(
+                    controller: valueController,
+                    // onChanged: (newValue) => title = newValue,
+                    decoration: InputDecoration(labelText: 'Valor (R\$)'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          print(valueController.text);
+                          print(titleController.text);
+                        },
+                        child: Text('Nova Transação'),
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.purple,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
